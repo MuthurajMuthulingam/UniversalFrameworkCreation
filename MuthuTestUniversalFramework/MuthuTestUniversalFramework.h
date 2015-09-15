f63a1192f52bcc4533744038d735416120ff7617
Muthuraj <muthurajm@karyatech.com>
2015-09-15 09:51:40 +0530

    Initial Commit

---
+++ MuthuTestUniversalFramework/MuthuTestUniversalFramework.h
@@ -0,0 +1,19 @@
+//
+//  MuthuTestUniversalFramework.h
+//  MuthuTestUniversalFramework
+//
+//  Created by Muthuraj M on 15/09/15.
+//  Copyright (c) 2015 Muthuraj M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MuthuTestUniversalFramework.
+FOUNDATION_EXPORT double MuthuTestUniversalFrameworkVersionNumber;
+
+//! Project version string for MuthuTestUniversalFramework.
+FOUNDATION_EXPORT const unsigned char MuthuTestUniversalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuthuTestUniversalFramework/PublicHeader.h>
+
+
---
+++ MuthuTestUniversalFrameworkTests/MuthuTestUniversalFrameworkTests.m
@@ -0,0 +1,40 @@
+//
+//  MuthuTestUniversalFrameworkTests.m
+//  MuthuTestUniversalFrameworkTests
+//
+//  Created by Muthuraj M on 15/09/15.
+//  Copyright (c) 2015 Muthuraj M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MuthuTestUniversalFrameworkTests : XCTestCase
+
+@end
+
+@implementation MuthuTestUniversalFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
